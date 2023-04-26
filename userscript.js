85c7e96c0c0abd9ca8c40b071590f37efdb77d85
Your Name <you@example.com>
2023-04-26 23:26:51 +0200

    continue vs return...

--- userscript.js
+++ userscript.js
@@ -5,6 +5,14 @@
 */
 
 
+/*
+<img alt="Kate Moss moment" class="hCL kVc L4E MIw" fetchpriority="auto" loading="auto" src="https://i.pinimg.com/236x/48/09/6a/48096a503ec12ef699601a0ae27d1749.jpg" srcset="https://i.pinimg.com/236x/48/09/6a/48096a503ec12ef699601a0ae27d1749.jpg 1x, https://i.pinimg.com/474x/48/09/6a/48096a503ec12ef699601a0ae27d1749.jpg 2x, https://i.pinimg.com/736x/48/09/6a/48096a503ec12ef699601a0ae27d1749.jpg 3x, https://i.pinimg.com/originals/48/09/6a/48096a503ec12ef699601a0ae27d1749.jpg 4x">
+
+"<img alt=\"Dies enthält ein Bild von: \" class=\"hCL kVc L4E MIw\" fetchpriority=\"auto\" loading=\"auto\" src=\"https://i.pinimg.com/236x/a5/5a/ee/a55aee676718053e6548c245f20df18e.jpg\" srcset=\"https://i.pinimg.com/236x/a5/5a/ee/a55aee676718053e6548c245f20df18e.jpg 1x, https://i.pinimg.com/474x/a5/5a/ee/a55aee676718053e6548c245f20df18e.jpg 2x, https://i.pinimg.com/736x/a5/5a/ee/a55aee676718053e6548c245f20df18e.jpg 3x, https://i.pinimg.com/originals/a5/5a/ee/a55aee676718053e6548c245f20df18e.jpg 4x\">"
+
+*/
+
+
 "use strict"
 var known_srcs = [];
 var zoom_level = "2x";
@@ -13,11 +21,12 @@ var my_target_url = "https://schizo.life/hot_links"
 
 setInterval(()=>{
 
-	let imgs = document.getElementsByTagName("img")
+	let imgs = document.getElementsByTagName("img");
+	console.log(imgs);
 	for(let i=0; i<imgs.length; i++){
 		let img = imgs[i];
 		if(!img.srcset){
-			return;
+			continue;
 		}
 
 		img = img.srcset.split(` `+zoom_level)[0];
